f4460f5c8cbcdb145542e4c108981cf3a6faddd3
haroldricafrente <ricafenteharold1@gmail.com>
2025-03-18 14:50:15 +0800

    try to fix websocket error

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ users_collection = db["users"]
 # Initialize Extensions
 bcrypt = Bcrypt(app)
 
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading', transports=["websocket"])
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
